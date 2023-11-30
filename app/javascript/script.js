8acedf710cfc2d5391f452c97548403ef70ec3cd
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-11-30 10:02:17 +0000

    [add]画像の表示・非表示機能

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -6,22 +6,61 @@
 //   document.getElementsByClassName('element')[1].style.display = "none"
 // })
 
-const elements = document.getElementsByClassName('element').style.display = "none"
-let len = elements.length;
-console.log(len)
+// const elements = document.getElementsByClassName('elem').style.display = "none"
+// let len = elements.length;
+// console.log(len)
 
-$(document).ready(function(){
-  $('.jquery').on('click', function(){
-    console.log('button clicked!');
-    $(this).css('color','red');
-  })
-})
+// const elements = document.getElementsByClassName('elem')
+// for(let i = 0; i < elements.length; i++){
+//   elements[i].style.display = "none";
+// }
+
+
+// $(document).ready(function(){
+//   $('.jquery').on('click', function(){
+//     console.log('button clicked!');
+//     $(this).css('color','red');
+//   })
+// })
+
+// $(document).ready(function(){
+//   $('.ruby-btn').on('click', function(){
+//     console.log('Button clicked!');
+//   $('.ruby').removeClass();
+//   })
+// })
+
+// $(document).ready(function(){
+//   $('.ruby-btn').on('click', function(){
+//     console.log('Button clicked!');
+//   $('#ruby').removeClass('inactive');
+//   })
+// })
 
 $(document).ready(function(){
   $('.ruby-btn').on('click', function(){
-    console.log('Button clicked!');
-   $('.ruby').removeClass();
+    console.log('button clicked!');
+    $('#ruby').toggle();
   })
 })
 
+$(document).ready(function(){
+  $('.santa_hat-btn').on('click', function(){
+    console.log('button clicked!');
+    $('#santa_hat').toggle();
+  })
+})
 
+$(document).ready(function(){
+  $('.necktie-btn').on('click', function(){
+    console.log('button clicked!');
+    $('#necktie').toggle();
+  })
+})
+
+$(document).ready(function(){
+  $('.sunglasses-btn').on('click', function(){
+    console.log('button clicked!');
+    $('#sunglasses').toggle();
+  })
+})
