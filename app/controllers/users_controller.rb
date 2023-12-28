8f414898c879685e0c71f5f35bf9d506e6490dbf
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-12-28 08:37:56 +0000

    [add]user作成。ユーザー画像もつけられる。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController
   
-def index
-  # 変数に画像を入れることもできる！
-  @image = "rubyman_shibainu.png"
-end 
-
-def show 
-    
-end 
+  def index
+    # 変数に画像を入れることもできる！
+    @image = "rubyman_shibainu.png"
+    @users = User.all
+  end 
   
+  def show 
+      
+  end 
+  
+  def create
+    User.create(user_params)
+    redirect_to users_path
+  end 
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :image)
+  end
+    
 end
