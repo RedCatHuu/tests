b499aaf3072d7780d12aa3f57af40312fb592fb1
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-12-29 07:52:38 +0000

    [add]ユーザー画像をminimagickで加工処理

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -7,13 +7,21 @@ class HomesController < ApplicationController
   
   def magick
     require 'mini_magick'
-    input_path = "rubyman_shibainu.png"
-    image = MiniMagick::Image.open(input_path)
+    # input_path = "rubyman_shibainu.png"
+    # image = MiniMagick::Image.open(input_path)
+    # image.resize "200x200"
+    # image.write "rubyman_shibainu.png"
+    # # disposition: "attachement"でダウンロードする。inlineはダウンロードせずに表示。
+    # send_file "rubyman_shibainu.png", type: "image/png", disposition: "inline"
+    # @magick = "rubyman_shibainu.png"
+    
+    ユーザー画像をminimagickで加工処理。
+    user_image = User.find(3).image
+    image_blob = user_image.download
+    image = MiniMagick::Image.read(image_blob)
     image.resize "100x100"
-    image.write "rubyman_shibainu.png"
-    # disposition: "attachement"でダウンロードする。inlineはダウンロードせずに表示。
-    send_file "rubyman_shibainu.png", type: "image/png", disposition: "inline"
-    @magick = "rubyman_shibainu.png"
+    send_data image.to_blob, type: "image/png", disposition: "inline"
+    
   end
   
 end
