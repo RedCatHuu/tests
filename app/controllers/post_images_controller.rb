2765807dfe7509b5c52e172bfe8f64d4dd5a2887
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-12-29 10:10:44 +0000

    [add]画像複数投稿可能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,20 +1,25 @@
 class PostImagesController < ApplicationController
   
   def index
-    
+    @post_images = PostImage.all
   end 
   
   def create
     post_image = PostImage.new(post_image_params)
     post_image.user_id = 3
-    post_image.save
-    redirect_to post_images_path
+    if post_image.save
+      redirect_to post_images_path
+    else
+      @post_images = PostImage.all
+      flash.now[:notice] = "失敗"
+      render :index
+    end
   end 
   
   private
   
   def post_image_params
-    params.require(:post_image).permit(:name, :images)
+    params.require(:post_image).permit(:name, :base_image, images: [])
   end 
   
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,7 @@
 class PostImage < ApplicationRecord
   belongs_to :user
+  has_one_attached :base_image
   has_many_attached :images
+  
+  validates :name, presence: true
 end
